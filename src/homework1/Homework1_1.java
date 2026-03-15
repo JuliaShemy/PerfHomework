675ec6b5508715ef81bb267168b591ff4a356cd8
JuliaShemy <Juliashem@gmail.com>
2026-03-15 23:50:13 +0400

    added Homework1

---
+++ src/homework1/Homework1_1.java
@@ -0,0 +1,12 @@
+package homework1;
+
+public class Homework1_1 {
+    public static void main(String[] args) {
+
+    int b = 2;
+    int c = 3;
+    int a = 4*(b+c-1)/2;
+
+    System.out.println("a=" + a);
+    }
+}
